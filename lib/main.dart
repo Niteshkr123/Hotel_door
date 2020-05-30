4ff9824d156fa8fff744493f9ed41881dd9d7d28
Nitesh Kumar <niteshkrraj2@gmail.com>
2020-05-30 13:07:47 +0530

    commit1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'login.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 
 void main() {
   runApp(MaterialApp(
